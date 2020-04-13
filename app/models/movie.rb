263f62cdfe34e8aedb6f131cb0041c110627eab6
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-14 01:40:38 +0900

    p73/Stateパターン導入

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,31 +1,22 @@
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
-
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
+  attr_writer :price
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    @price = value
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2 ) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
