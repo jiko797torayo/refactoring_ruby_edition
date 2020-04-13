0defa0fcf2c9950c92ff7ab149d74d4f114baa94
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-14 00:27:13 +0900

    p54/html_statementメソッド追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -21,6 +21,19 @@ class Customer
     result
   end
 
+  def html_statement
+    result = "<h1>Rental Record for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # このレンタル料金を表示
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "<br>\n"
+    end
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+      "frequent renter points<p>"
+    result
+  end
+
   private
 
   def total_charge
