6865797739cc762bca1312b3d6799e27e4398a3b
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-14 00:19:57 +0900

    p51/一時変数から問い合わせメソッドへ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,16 +11,13 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-
       # このレンタル料金を表示
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
@@ -28,5 +25,10 @@ class Customer
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
+
   end
 end
